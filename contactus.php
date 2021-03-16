712efda428c514d9ee6005a3cecc9b91193ba23e
Kavya-uday123 <mca522@rajagiri.edu>
2021-03-16 22:10:53 +0530

    Restaurant

--- contactus.php
+++ contactus.php
@@ -48,5 +48,34 @@
 &nbsp&nbsp&nbsp&nbsp&nbsp&nbspWeekends |  09:00 - 01:00<br>
     </p>
  </div>
+ <div class="row" style="background-color:lightyellow;">
+        <h4><center>Write To Us</center></h4>
+        <table class="table" align="center">
+            <tr>
+                <td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspName</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspEmail</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspMobile No</td>
+                <td><input type="number" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSubject</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspDescription</td>
+                <td><input type="text" class="form-control"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button class="btn btn-dark">SUBMIT</button></td>
+            </tr>
+        </table>
+     </div>
 </body>
 </html>
